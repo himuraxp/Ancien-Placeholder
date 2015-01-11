8d8ef3ec183b39cfe44f720052c774056e562aeb
himuraxp <himuraxp@users.noreply.github.com>
2015-01-11 12:16:03 +0100

    Orthographe

--- NombreMystere.c
+++ NombreMystere.c
@@ -1,6 +1,6 @@
 /*Jeu en mode console PLUS OU MOINS. 
  * Le but de ce jeu est de trouver le NOMBRE MYSTERE. 
- * Il sera automatiquement générer en SOLO ou demander par un joueur supplementaire en VERSUS. 
+ * Il sera automatiquement généré en SOLO ou demandé par un joueur supplementaire en VERSUS. 
  * La console indiquera plus ou moins pour aider le joueur à retrouver le NOMBRE MYSTERE. 
  * Plusieurs modes de difficultés sont disponibles.*/
 
