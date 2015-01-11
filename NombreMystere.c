67a6ba1ddfcb098993c66512121b9ac95f46510e
HimuraXp <larbi_yohann@hotmail.com>
2015-01-11 12:10:59 +0100

    Description du jeu

--- NombreMystere.c
+++ NombreMystere.c
@@ -1,3 +1,9 @@
+/*Jeu en mode console PLUS OU MOINS. 
+ * Le but de ce jeu est de trouver le NOMBRE MYSTERE. 
+ * Il sera automatiquement générer en SOLO ou demander par un joueur supplementaire en VERSUS. 
+ * La console indiquera plus ou moins pour aider le joueur à retrouver le NOMBRE MYSTERE. 
+ * Plusieurs modes de difficultés sont disponibles.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
